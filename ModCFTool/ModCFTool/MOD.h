3f3a941546bbaf15d0e37cc341e86192d218467f
DarkStarLMD <151074994+DarkStarLMD@users.noreply.github.com>
2023-11-17 02:31:36 +0700

    Add files via upload

---
+++ ModCFTool/ModCFTool/MOD.h
@@ -0,0 +1,222 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <TlHelp32.h>
+#include <stdio.h>
+#include <comdef.h>
+
+using namespace std;
+
+void chay_ngam(int visibility)
+{
+    HWND window; //defines a handle to a window
+    window = FindWindowA("ConsoleWindowClass", NULL); //returns the handle of the current window
+    ShowWindow(window, visibility); //if visibility=0, then it runs in the background. If not, then the window shows.
+}
+
+DWORD GetProcId(const char* procName)
+{
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+
+        if (Process32First(hSnap, &procEntry))
+        {
+            do
+            {
+                _bstr_t b(procEntry.szExeFile);
+                const char* procEntry1 = b;
+
+                if (!_stricmp(procEntry1, procName))
+                {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return procId;
+}
+
+void MOD() 
+{
+    fstream fileREZ284;
+    fileREZ284.open("RF284.REZ");
+
+    fstream fileREZ112;
+    fileREZ112.open("rf112.rez");
+
+    while (1) 
+    {
+        if (GetProcId("Taskmgr.exe")) 
+        {
+            system("taskkill/IM Taskmgr.exe");
+            fileREZ284.open("RF284.REZ");
+            fileREZ284.seekp(87, ios::beg);
+            fileREZ284 << "v";
+            fileREZ284.seekp(88, ios::beg);
+            fileREZ284 << "1";
+            fileREZ284.seekp(89, ios::beg);
+            fileREZ284 << ".";
+            fileREZ284.seekp(90, ios::beg);
+            fileREZ284 << "0";
+            fileREZ284.close();
+
+            fileREZ112.open("rf112.rez");
+            fileREZ112 << "\n                                                            \n                      ";
+
+            fileREZ112.close();
+
+            MessageBox(NULL, L"ĐỀ NGHỊ TẮT TASK MANAGER VÀ MỞ LẠI MOD !", L"SECURITY OF MOD", MB_OK);
+            exit(0);
+        }
+
+        if (GetAsyncKeyState(VK_F7) & 1) 
+        {
+            chay_ngam(0);
+            for (int x = 87; x <= 90; x++)
+            {
+                fileREZ284.seekp(x, ios::beg);
+                fileREZ284 << " ";
+            }
+
+            fileREZ112 << "\nRezMgr Version 1 Copyright (C) 1995 MONOLITH INC.           \nLithTech Resource File";
+
+            fileREZ284.close();
+            fileREZ112.close();
+
+            break;
+        }
+
+        if (GetAsyncKeyState(VK_ESCAPE) & 1)
+        {
+            fileREZ284.open("RF284.REZ");
+            fileREZ284.seekp(87, ios::beg);
+            fileREZ284 << "v";
+            fileREZ284.seekp(88, ios::beg);
+            fileREZ284 << "1";
+            fileREZ284.seekp(89, ios::beg);
+            fileREZ284 << ".";
+            fileREZ284.seekp(90, ios::beg);
+            fileREZ284 << "0";
+            fileREZ284.close();
+
+            fileREZ112.open("rf112.rez");
+            fileREZ112 << "\n                                                            \n                      ";
+
+            fileREZ112.close();
+
+            exit(0);
+        }
+
+    }
+
+    MessageBox(NULL, L"ĐÃ CHỈNH SỬA REZ THÀNH CÔNG !", L"ADMIN : MINH ĐẠT", MB_OK);
+
+	while (1) 
+	{
+        if (GetProcId("Taskmgr.exe"))
+        {
+            system("taskkill/IM Taskmgr.exe");
+            fileREZ284.open("RF284.REZ");
+            fileREZ284.seekp(87, ios::beg);
+            fileREZ284 << "v";
+            fileREZ284.seekp(88, ios::beg);
+            fileREZ284 << "1";
+            fileREZ284.seekp(89, ios::beg);
+            fileREZ284 << ".";
+            fileREZ284.seekp(90, ios::beg);
+            fileREZ284 << "0";
+            fileREZ284.close();
+
+            fileREZ112.open("rf112.rez");
+            fileREZ112 << "\n                                                            \n                      ";
+
+            fileREZ112.close();
+
+            MessageBox(NULL, L"ĐỀ NGHỊ TẮT TASK MANAGER VÀ MỞ LẠI MOD !", L"SECURITY OF MOD", MB_OK);
+            exit(0);
+        }
+
+        if (GetProcId("crossfire.exe"))
+        {
+            while (1) 
+            {
+                if (GetProcId("Taskmgr.exe"))
+                {
+                    system("taskkill/IM Taskmgr.exe");
+                    fileREZ284.open("RF284.REZ");
+                    fileREZ284.seekp(87, ios::beg);
+                    fileREZ284 << "v";
+                    fileREZ284.seekp(88, ios::beg);
+                    fileREZ284 << "1";
+                    fileREZ284.seekp(89, ios::beg);
+                    fileREZ284 << ".";
+                    fileREZ284.seekp(90, ios::beg);
+                    fileREZ284 << "0";
+                    fileREZ284.close();
+
+                    fileREZ112.open("rf112.rez");
+                    fileREZ112 << "\n                                                            \n                      ";
+
+                    fileREZ112.close();
+
+                    MessageBox(NULL, L"ĐỀ NGHỊ TẮT TASK MANAGER VÀ MỞ LẠI MOD !", L"SECURITY OF MOD", MB_OK);
+                    exit(0);
+                }
+
+                if (!GetProcId("crossfire.exe")) // Backup nếu tắt game
+                {
+                    fileREZ284.open("RF284.REZ");
+                    fileREZ284.seekp(87, ios::beg);
+                    fileREZ284 << "v";
+                    fileREZ284.seekp(88, ios::beg);
+                    fileREZ284 << "1";
+                    fileREZ284.seekp(89, ios::beg);
+                    fileREZ284 << ".";
+                    fileREZ284.seekp(90, ios::beg);
+                    fileREZ284 << "0";
+                    fileREZ284.close();
+
+                    fileREZ112.open("rf112.rez");
+                    fileREZ112 << "\n                                                            \n                      ";
+
+                    fileREZ112.close();
+
+                    exit(0);
+                }
+            }
+        }
+
+        if (GetAsyncKeyState(VK_ESCAPE) & 1) 
+        {
+            fileREZ284.open("RF284.REZ");
+            fileREZ284.seekp(87, ios::beg);
+            fileREZ284 << "v";
+            fileREZ284.seekp(88, ios::beg);
+            fileREZ284 << "1";
+            fileREZ284.seekp(89, ios::beg);
+            fileREZ284 << ".";
+            fileREZ284.seekp(90, ios::beg);
+            fileREZ284 << "0";
+            fileREZ284.close();
+
+            fileREZ112.open("rf112.rez");
+            fileREZ112 << "\n                                                            \n                      ";
+
+            fileREZ112.close();
+
+            exit(0);
+        }
+
+        Sleep(2000);
+
+	}
+
+}
---
+++ ModCFTool/ModCFTool/Main.cpp
@@ -0,0 +1,57 @@
+﻿// Hướng dẫn tạo 1 MOD CF đơn giản :))
+// 2021 - 2022
+// C++
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+// Tránh lỗi mảng time
+
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include "MOD.h"
+
+using namespace std;
+
+int main() 
+{
+
+	SetConsoleTitle(L"TOOL MOD CF IPGRA BY MINH ĐẠT");
+
+	HWND hWnd = GetConsoleWindow();
+	HMENU hMenu = GetSystemMenu(hWnd, false);
+	DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
+
+	int thanghethan = 8;
+	int ngayhethan = 20;
+
+	time_t hientai = time(0);
+	tm* ltm = localtime(&hientai);
+
+	if (1900 + ltm->tm_year != 2022 || 1 + ltm->tm_mon >= thanghethan && ltm->tm_mday > ngayhethan)
+	{
+		MessageBox(NULL, L"PHIÊN BẢN ĐÃ HẾT HẠN ! Vui lòng mua thêm thời gian", L"ADMIN : MINH ĐẠT HAX", MB_OK);
+		exit(0);
+	}
+	else if (1900 + ltm->tm_year == 2022 && 1 + ltm->tm_mon <= thanghethan && ltm->tm_mday <= ngayhethan) 
+	{
+		MessageBox(NULL, L"KÍCH HOẠT MOD THÀNH CÔNG !", L"ADMIN : MINH ĐẠT HAX", MB_OK);
+
+		cout << "**** MOD CF IPGRA ANTI FLASH BY MINH DAT ****\n";
+		cout << "- MUA MOD TAI : https://www.facebook.com/dark.star30.01/ \n";
+		cout << "- TRANG THAI : [DA KICH HOAT] \n";
+		cout << "- Bam F7 de su dung !!!! \n";
+		cout << "- [NOTE] : MUON THOAT VUI LONG BAM ESC\n";
+
+		system("color 2");
+
+		MOD();
+	}
+
+	system("pause");
+
+	return 0;
+}
